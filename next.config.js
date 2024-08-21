47ebd7814eab90ea05ed424aff7d082af71250a9
ShenCiao <3294833183@qq.com>
2024-08-21 21:54:55 +0800

    Configurations

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withOffline = require('next-offline');
 const nextConfig = {
 	poweredByHeader: false,
 	swcMinify: true,
+	reactStrictMode: true,
+	basePath: "/shenciao",
 };
 
 module.exports = withOffline(nextConfig);
